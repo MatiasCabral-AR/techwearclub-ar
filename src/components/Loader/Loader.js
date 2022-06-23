8e26b7312f1943d5267e9b1a55f8e26b51bad8a6
pagatelabirra <raulmatiascabral@gmail.com>
2022-06-22 21:17:53 -0300

    Created Loader, changed name of Error component to ErrorRender

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,8 @@
+import './Loader.css'
+import Container from 'react-bootstrap/esm/Container'
+
+export const Loader = () => {
+    <Container className='d-flex justify-content-center'>
+        <div className='loading f-futurism display-2' data-loading-text='TechwearclubAR'></div>
+    </Container>
+}
